9b877d1928f6e2a43ad949f40f9c6feab7ac1059
Pureheart Moluno <businesspmoluno@gmail.com>
2022-01-11 08:25:59 +0100

    final version

--- app.js
+++ app.js
@@ -52,6 +52,6 @@ app.get("*", (req, res) => {
   res.redirect("/");
 });
 // SERVER
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT , () => {
   console.log("App Started");
 });
