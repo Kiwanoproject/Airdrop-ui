17439eca0857a1ca1d944c5e4954a50b0440e289
Pureheart Moluno <businesspmoluno@gmail.com>
2022-01-18 01:02:01 +0100

    final version

--- routes/airdrop.js
+++ routes/airdrop.js
@@ -8,7 +8,7 @@ router.get("/", async (req, res) => {
   const title = "The Kiwano Project - Airdrops";
   const description ="The Kiwano project official airdrop page. The kiwano airdrop program is live.";
   const wallet = req.query; 
-  const ref = await Participant.findOne(wallet);
+  const ref = await Participant.findOne(wallet).lean();
   if (ref == null) {
     const referre = "1";
     const refNum = "";
@@ -16,7 +16,7 @@ router.get("/", async (req, res) => {
     const balance = 0;
     res.render("airdrop", { refNum, referre, refLink, balance, description, title});
   } else {
-    const refNum = await Participant.find({ referre: ref.referral });
+    const refNum = await Participant.find({ referre: ref.referral }).lean();
     if (Object.entries(wallet).length === 0) {
       const referre = "1";
       const refNum = "";
@@ -24,7 +24,7 @@ router.get("/", async (req, res) => {
       const balance = 0;
       res.render("airdrop", { refNum, referre, refLink, balance, title, description });
     } else {
-      const detail = await Participant.findOne(wallet);
+      const detail = await Participant.findOne(wallet).lean();
       const referre = detail.referral;
       const refLink = detail.referral;
       const balance = 10;
@@ -41,10 +41,10 @@ router.get("/", async (req, res) => {
   }
 });
 router.post("/", async (req, res) => {
-  const checkMail = await Participant.findOne({ email: req.body.email });
-  const checkWallet = await Participant.findOne({ wallet: req.body.wallet });
-  const checkTwitter = await Participant.findOne({ twitter: req.body.twitter });
-  const checkTelegram = await Participant.findOne({ telegram: req.body.telegram });
+  const checkMail = await Participant.findOne({ email: req.body.email }).lean();
+  const checkWallet = await Participant.findOne({ wallet: req.body.wallet }).lean();
+  const checkTwitter = await Participant.findOne({ twitter: req.body.twitter }).lean();
+  const checkTelegram = await Participant.findOne({ telegram: req.body.telegram }).lean();
       if (checkMail != null || checkWallet != null || checkTwitter != null || checkTelegram != null ) {
         req.flash('success_msg','You Already Participated!');
         res.redirect("/airdrop");
@@ -72,9 +72,9 @@ router.get("/:referral", async (req, res) => {
   const title = "The Kiwano Project - Airdrops";
   const description ="The Kiwano project official airdrop page. The kiwano airdrop program is live.";
   const wallet = req.query;
-  const ref = await Participant.find(wallet);
-  const refNum = await Participant.find({ referre: ref.referral });
-  const detail = await Participant.findOne(req.params);
+  const ref = await Participant.find(wallet).lean();
+  const refNum = await Participant.find({ referre: ref.referral }).lean();
+  const detail = await Participant.findOne(req.params).lean();
   if (detail !== null) {
     const referre = detail.referral;
     const refLink = "";
